c2d79f4c2f1df12d06c6a31d9524348fb1205051
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-13 17:18:25 -0800

    PlayerCard created, fetching data from api, displays player data via that api

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PlayerCard extends Component {
-    constructor(){
-        super();
-        this.state = {
-        }
+    // constructor(){
+    //     super();
+    //     this.state = {
+                // players:[]
+    //     }
+    // }
+    state = {
+        players: []
+    }
+
+    componentDidMount(){
+        axios   
+            .get(`http://localhost:5000/api/players`)
+            .then(result => {
+                console.log(result)
+                this.setState({players: result.data})
+            })
+            .catch(error => {
+                console.log('Error, oh to error:', error)
+            })
     }
 
     render() {
+        const { players } = this.state;
+        console.log(this.state)
         return (
             <>
-            <h4>from PlayerCard</h4>
+                <h4>from PlayerCard</h4>
+                {players.map(playing => {
+                    {console.log(playing)}
+                    return(
+                        <div>
+                            <p>Name: {playing.name}</p>
+                            <p>Country:{playing.country} </p>
+                            <p>Searches: {playing.searches} </p>
+                        </div>
+                    )
+                })}
             </>
         )
     }
