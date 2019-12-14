2211a1819bf665737a8ad70b1b1109736a59b353
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-13 18:08:23 -0800

    created useLocalStorage and useDarkMode

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 const NavBar = () => {
+    const [darkMode, setDarkMode] = useDarkMode();
+    const toggleMode = event => {
+        event.preventDefault();
+        setDarkMode(!darkMode)
+    }
 
     return (
         <div>
-            <h4>2019 Women's World Cup</h4>
-            <p>Players ranked by search interest from Google Trends</p>
-            <h5>June-July 2019, worldwide</h5>
+            <div className='toggle_dark-mode'>
+                <button 
+                    className={darkMode ? 'toggle toggled' : 'toggle'}
+                    onClick={toggleMode}
+                >Button</button>
+            </div>
+            <div>
+                <h4>2019 Women's World Cup</h4>
+                <p>Players ranked by search interest from Google Trends</p>
+                <h5>June-July 2019, worldwide</h5>
+            </div>
         </div>
     )
 }
