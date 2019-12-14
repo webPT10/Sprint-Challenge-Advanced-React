490925088215de10cde47bcca6b9e5846d87e0e3
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-12-13 16:25:53 -0800

    constructed skeleton for PlayerCard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import PlayerCard from './components/PlayerCard';
+
 function App() {
   return (
     <div className="App">
-      
+      <PlayerCard />
     </div>
   );
 }
---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class PlayerCard extends Component {
+    constructor(){
+        super();
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <>
+            <h4>from PlayerCard</h4>
+            </>
+        )
+    }
+}
+export default PlayerCard;
